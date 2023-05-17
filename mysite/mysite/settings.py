73aecfcc8ba3df10e9ab6a0633af58c55820f9ec
Piotrulaa <62243934+Piotrulaa@users.noreply.github.com>
2023-05-17 16:37:44 +0200

    Added template content for post/list and post/details + basic CSS (#2)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "cq*oz6-ed=8l*)=nhqvg)*50@gbu-(lsmahw974c&_e9#27a*b"
+SECRET_KEY = os.environ.get("DJANGO_BLOG_APP_SECRET_KEY")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
