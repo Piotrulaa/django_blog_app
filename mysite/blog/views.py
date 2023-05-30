e0a28d0f66e09bfdc6aba05ac1c95995341e20b9
Piotrulaa <62243934+Piotrulaa@users.noreply.github.com>
2023-05-31 00:22:23 +0200

    Added sharing posts via emails (#5)

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 # from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 class PostListView(ListView):
@@ -31,6 +34,36 @@ class PostListView(ListView):
 #             "posts": posts
 #         }
 #     )
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status="published")
+    sent = False
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = "{} ({}) zachęca do przeczytania \"{}\"".format(
+                cd["name"], cd["email"], post.title
+            )
+            message = "Przeczytaj post \"{}\" na stronie {}\n\nKomentarz dodany przez {}: {}".format(
+                post.title, post_url, cd["name"], cd["comments"]
+            )
+            send_mail(subject, message, settings.EMAIL_SENDER_ADDRESS, [cd["to"]])
+            sent = True
+    else:
+        # If request method is not POST, return empty form
+        form = EmailPostForm()
+    return render(
+        request,
+        template_name="blog/post/share.html",
+        context={
+            "post": post,
+            "form": form,
+            "sent": sent,
+        }
+    )
 
 
 def post_detail(request, year, month, day, post):
