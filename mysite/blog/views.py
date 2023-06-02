115f218b2030da7b4a4780eb3a58a7209c2a6ed8
Piotrulaa <62243934+Piotrulaa@users.noreply.github.com>
2023-06-03 01:48:33 +0200

    Added tags for blog posts (#7)

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,39 +1,45 @@
 from django.shortcuts import render, get_object_or_404
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post, Comment
-from django.views.generic import ListView
+# from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.conf import settings
+from taggit.models import Tag
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = "posts"
-    paginate_by = 3
-    template_name = "blog/post/list.html"
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = "posts"
+#     paginate_by = 3
+#     template_name = "blog/post/list.html"
 
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)  # 3 posts per page
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         # If page is not int, return first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # If page is greater than total number of pages, return last page
-#         posts = paginator.page(paginator.num_pages)
-#     return render(
-#         request,
-#         template_name="blog/post/list.html",
-#         context={
-#             "page": page,
-#             "posts": posts
-#         }
-#     )
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)  # 3 posts per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not int, return first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is greater than total number of pages, return last page
+        posts = paginator.page(paginator.num_pages)
+    return render(
+        request,
+        template_name="blog/post/list.html",
+        context={
+            "page": page,
+            "posts": posts,
+            "tag": tag,
+        }
+    )
 
 
 def post_share(request, post_id):
